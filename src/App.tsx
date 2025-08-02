77500a8bf0f8a7be42e60f4f709064972d50f21f
BadriAhmed <abadri90@gmail.com>
2025-08-02 23:50:10 +0200

    Refresh same page stay

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ const AppRoutes: React.FC = () => {
           </ProtectedRoute>
         }
       />
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
 };
